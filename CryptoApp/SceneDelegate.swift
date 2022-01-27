76e24b13a63a141e6605ce5d5364c613fa8b4d4e
Ed <Siniakov>
2022-01-27 22:30:21 +0300

    только светлая тема

--- CryptoApp/SceneDelegate.swift
+++ CryptoApp/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.overrideUserInterfaceStyle = .dark
+        window.overrideUserInterfaceStyle = .light
+//        window.overrideUserInterfaceStyle = .dark
         let tabBar = TabBarVC()
         window.rootViewController = tabBar
         self.window = window
