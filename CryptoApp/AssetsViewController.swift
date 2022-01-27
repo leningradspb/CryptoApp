f017b29ceedac2dfe8d4c883d73cf94b33f93172
Ed <Siniakov>
2022-01-27 23:41:49 +0300

    searchController

--- CryptoApp/AssetsViewController.swift
+++ CryptoApp/AssetsViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 final class AssetsViewController: UIViewController {
-
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
@@ -20,13 +21,8 @@ final class AssetsViewController: UIViewController {
         navigationItem.title = LocalizationNames.assets
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
-//        self.navigationController?.navigationBar.backgroundColor = .white
-        
-//        guard let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView else { return }
-//
-//        statusBar.backgroundColor = .blue
-//        navigationController?.navigationBar.isTranslucent = false
-       
+        searchController.obscuresBackgroundDuringPresentation = true
+        navigationItem.searchController = searchController
     }
 
 
