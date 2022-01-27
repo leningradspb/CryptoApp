b9d1573a7c5124ba7e9155b7369ce393d6747b33
Ed <Siniakov>
2022-01-27 21:09:06 +0300

    start

---
+++ CryptoApp/TabBarVC.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarVC.swift
+//  CryptoApp
+//
+//  Created by Eduard Sinyakov on 27.01.2022.
+//
+
+import UIKit
+
+final class TabBarVC: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        var tabBarList: [UIViewController]!
+        
+        let assetsNC = UINavigationController()
+        let assetsVC = AssetsViewController()
+        assetsNC.viewControllers = [assetsVC]
+        
+        let bubbleIcon = UIImage(named: "bitcoinsign.circle.fill")
+
+        assetsVC.tabBarItem.title = "Assets"
+        assetsVC.tabBarItem.image = bubbleIcon
+        assetsVC.tabBarItem.tag = 0
+        
+        tabBar.backgroundColor = .white
+        tabBarList = [assetsNC]
+        viewControllers = tabBarList
+    }
+}
